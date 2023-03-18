8f58f772bb15eacea6f3ed58d7a0b981414e39b3
mixail <araiz5000@gmail.com>
2023-03-18 10:58:00 +0300

    7lab342

---
+++ app/src/main/java/com/example/newapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "e8abadb800004c119cfe441978568700"
+    }
+}
